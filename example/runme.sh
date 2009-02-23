f40b73582f813292cf6115d0048cc6ababd8ae22
roberto.alsina <roberto.alsina@6f9045a4-009a-11de-88c7-29a3b14d5316>
2009-02-23 09:38:12 +0000

    Make it work better

--- example/runme.sh
+++ example/runme.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+mkdir out 2>/dev/null
 python ../rst2qhc.py handbook.txt reference.txt -o out --namespace urssus --filterattributes urssus:0.2.13 --rst2htmlopts="--stylesheet=style.css" --manifest MANIFEST
-
+cd out
+qcollectiongenerator project.qhcp -o collection.qhc
+assistant -collectionFile collection.qhc -register doc.qhc 
+assistant -collectionFile collection.qhc&
+cd ..
